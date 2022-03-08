793d3b002eb9295586b1fafcd825c349eca90751
LijeshPJaison <lijeshpjaison7@gmail.com>
2022-03-08 10:04:01 +0530

    upload my project

---
+++ app11/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from.import views
+
+urlpatterns = [
+    path('insex3', views.home),
+    path('index2',views.index2)
+]
---
+++ app11/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return HttpResponse('haii')
+
+def index2(request):
+    return render(request,'temp.html')
+
